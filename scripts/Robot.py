c7680bfdfaec77604259fcd6e791420c8aba60c6
Vigrel <eller.vini@gmail.com>
2021-05-11 17:32:49 -0300

    Funcionamento de Garras

--- scripts/Robot.py
+++ scripts/Robot.py
@@ -15,185 +15,220 @@ from cv_bridge import CvBridge, CvBridgeError
 from subprocess import call
 from colorama import Fore, Back, Style
 from colorama import init
+from std_msgs.msg import Float64
 
 bridge = CvBridge()
 
 def segment_yellow_line(hsv):
-    low = np.array([25, 150, 150])
-    high = np.array([35, 255, 255])
-    mask = cv2.inRange(hsv, low, high)
-    return mask
+	low = np.array([25, 150, 150])
+	high = np.array([35, 255, 255])
+	mask = cv2.inRange(hsv, low, high)
+	return mask
 
 def morpho_clean(mask):
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(6,6))
-    mask = cv2.morphologyEx( mask, cv2.MORPH_OPEN, kernel )
-    mask = cv2.morphologyEx( mask, cv2.MORPH_CLOSE, kernel )    
-    return mask
+	kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(6,6))
+	mask = cv2.morphologyEx( mask, cv2.MORPH_OPEN, kernel )
+	mask = cv2.morphologyEx( mask, cv2.MORPH_CLOSE, kernel )    
+	return mask
 
 class Robot:
-    def __init__(self):
-        self.velocity = Twist()
-        self.image = None
-        self.image_height = None
-        self.image_width = None
-        self.ranges = None
-        
-        self.aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
-        self.vel_publisher = rospy.Publisher(
+	def __init__(self):
+		self.velocity = Twist()
+		self.image = None
+		self.image_height = None
+		self.image_width = None
+		self.ranges = None
+
+		self.pos_braco = Float64()
+		self.pos_braco.data = -0.4
+		self.pos_garra = Float64()
+		self.pos_garra.data = -1
+		
+		self.aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
+		self.vel_publisher = rospy.Publisher(
 			'/cmd_vel',
 			Twist, 
 			queue_size=3)
-        self.laser_subscriber = rospy.Subscriber(
+		self.laser_subscriber = rospy.Subscriber(
 			'/scan', 
 			LaserScan, 
 			self.laser_callback)
-        self.image_subscriber = rospy.Subscriber(
-            "/camera/image/compressed", 
-            CompressedImage, 
-            self.image_callback, 
-            queue_size=4, 
-            buff_size = 2**24)
-
-    
-    def laser_callback(self, dado):
-        ranges = np.array(dado.ranges).round(decimals=2)
-        self.setRanges(ranges)
-    
-    def image_callback(self, imagem):
-        try:
-            cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
-
-            self.setImage(cv_image)
-            
-            cv2.waitKey(1)
-        except CvBridgeError as e:
-            print('ex', e)
-    
-    
-    def publish_in_cmd_vel(self):
-        self.vel_publisher.publish(self.velocity)
-    
-    def getVelocity(self):
-        return self.velocity
-    
-    def setVelocity(self, vel_lin, vel_ang):
-        self.velocity.linear.x = vel_lin
-        self.velocity.angular.z = vel_ang
-    
-    def getRanges(self, angle = None):
-        if angle != None:
-            return self.ranges[angle]
-        return self.ranges
-    
-    def setRanges(self, new_ranges):
-        self.ranges = new_ranges
-    
-    def setImage(self, new_image):
-        self.image = new_image
-        h, w, d = new_image.shape
-        self.image_height = h
-        self.image_width = w
-    
-    def getImage(self, gray = False, rgb = False, hsv = False, original = False):
-        if gray:
-            return cv2.cvtColor(self.image.copy(), cv2.COLOR_BGR2GRAY)
-        elif rgb:
-            return cv2.cvtColor(self.image.copy(), cv2.COLOR_BGR2RGB)
-        elif hsv:
-            return cv2.cvtColor(self.image.copy(), cv2.COLOR_BGR2HSV)
-        elif original:
-            return self.image
-
-        return self.image.copy()
-    
-    def getWidth(self):
-        return self.image_width
-    
-    def getHeight(self):
-        return self.image_height
-    
-    def getAruco(self, draw = False):
-        corners, ids, rejectedImgPoints = aruco.detectMarkers(self.getImage(gray=True), self.aruco_dict)
-        img = self.getImage()
-
-        if draw:
-            aruco.drawDetectedMarkers(img, corners, ids)
-        
-        ids = np.squeeze(ids)
-
-        return ids, img
-
-    def get_yellow_moments(self, img = None):
-
-        if img is None:
-            img = self.getImage(hsv=True)
-
-        mask = segment_yellow_line(img)
-        mask = morpho_clean(mask)
-
-        M = cv2.moments(mask)
-
-        return M
+		self.image_subscriber = rospy.Subscriber(
+			"/camera/image/compressed", 
+			CompressedImage, 
+			self.image_callback, 
+			queue_size=4, 
+			buff_size = 2**24)
+		self.braco_publisher = rospy.Publisher(
+			'/joint1_position_controller/command',
+			Float64,
+			queue_size=1)
+		self.garra_publisher = rospy.Publisher(
+			'/joint2_position_controller/command',
+			Float64,
+			queue_size=1)
+		
 
 
-    def follow_yellow_line(self, min_vel_linear = 0.2, max_vel_linear = 0.5, img = None):
+	# movimentacao da garra
+	def move_joints_init(self):
+		
+		
+		self.braco_publisher.publish(pos_braco)
+		self.garra_publisher.publish(pos_garra)
+	
+	def move_joints(self, braco, garra):
+		pos_braco = Float64()
+		pos_braco.data = braco
+		pos_garra = Float64()
+		pos_garra.data = garra
+		
+		self.braco_publisher.publish(pos_braco)
+		self.garra_publisher.publish(pos_garra)
 
-        if img is None:
-            img = self.getImage(hsv=True)
+	##################################
+	def laser_callback(self, dado):
+		ranges = np.array(dado.ranges).round(decimals=2)
+		self.setRanges(ranges)
+	
+	def image_callback(self, imagem):
+		try:
+			cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
 
-        M = self.get_yellow_moments(img)
+			self.setImage(cv_image)
+			
+			cv2.waitKey(1)
+		except CvBridgeError as e:
+			print('ex', e)
+	
+	
+	def publish_in_cmd_vel(self):
+		self.vel_publisher.publish(self.velocity)
+	
+	def getVelocity(self):
+		return self.velocity
+	
+	def setVelocity(self, vel_lin, vel_ang):
+		self.velocity.linear.x = vel_lin
+		self.velocity.angular.z = vel_ang
+	
+	def getRanges(self, angle = None):
+		if angle != None:
+			return self.ranges[angle]
+		return self.ranges
+	
+	def setRanges(self, new_ranges):
+		self.ranges = new_ranges
+	
+	def setImage(self, new_image):
+		self.image = new_image
+		h, w, d = new_image.shape
+		self.image_height = h
+		self.image_width = w
+	
+	def getImage(self, gray = False, rgb = False, hsv = False, original = False):
+		if gray:
+			return cv2.cvtColor(self.image.copy(), cv2.COLOR_BGR2GRAY)
+		elif rgb:
+			return cv2.cvtColor(self.image.copy(), cv2.COLOR_BGR2RGB)
+		elif hsv:
+			return cv2.cvtColor(self.image.copy(), cv2.COLOR_BGR2HSV)
+		elif original:
+			return self.image
 
-        if M['m00'] > 0:
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
+		return self.image.copy()
+	
+	def getWidth(self):
+		return self.image_width
+	
+	def getHeight(self):
+		return self.image_height
+	
+	def getAruco(self, draw = False):
+		corners, ids, rejectedImgPoints = aruco.detectMarkers(self.getImage(gray=True), self.aruco_dict)
+		img = self.getImage()
 
-            cv2.circle(self.getImage(original=True), (cx, cy), 10, (0,0,255), -1)
+		if draw:
+			aruco.drawDetectedMarkers(img, corners, ids)
+		
+		ids = np.array(ids).flatten()
 
-            err = cx - self.getWidth()/2
-            
-            if abs(err) > 10:
-                vel_linear = min_vel_linear
-            else:
-                vel_linear = max_vel_linear
+		return ids, img
 
-            vel_ang = -float(err) / 100
+	def get_yellow_moments(self, img = None):
 
-            self.setVelocity(vel_linear, vel_ang)
+		if img is None:
+			img = self.getImage(hsv=True)
 
-        self.publish_in_cmd_vel()
+		mask = segment_yellow_line(img)
+		mask = morpho_clean(mask)
 
-    
-    def rotate(self, vel_ang = math.pi/10, clockwise = False):
-        if clockwise:
-            self.setVelocity(0, -vel_ang)
-        else:
-            self.setVelocity(0, vel_ang)
-        
-        self.publish_in_cmd_vel()
-    
-    def go_to_creeper(self, creeper, min_distance = 0.3):
+		M = cv2.moments(mask)
 
-        media, centro, maior_area = creeper.identify_color(self)
+		return M
 
-        crashed = True if self.getRanges(0) < min_distance else False
 
-        if len(media) != 0 and len(centro) != 0:
-            if abs(media[0] - centro[0]) > 50:
-                if (media[0] > centro[0]):
-                    self.setVelocity(0, -0.1)
-                if (media[0] < centro[0]):
-                    self.setVelocity(0, 0.1)
-            else:
-                if not crashed:
-                    if (media[0] > centro[0]):
-                        self.setVelocity(0.1, -0.1)
-                    if (media[0] < centro[0]):
-                        self.setVelocity(0.1, 0.1)
-        
-        if crashed:
-            self.setVelocity(0, 0)
+	def follow_yellow_line(self, min_vel_linear = 0.2, max_vel_linear = 0.5, img = None):
 
-        self.publish_in_cmd_vel()
+		if img is None:
+			img = self.getImage(hsv=True)
 
-        return crashed
-    
+		M = self.get_yellow_moments(img)
+
+		if M['m00'] > 0:
+			cx = int(M['m10']/M['m00'])
+			cy = int(M['m01']/M['m00'])
+
+			cv2.circle(self.getImage(original=True), (cx, cy), 10, (0,0,255), -1)
+
+			err = cx - self.getWidth()/2
+			
+			if abs(err) > 10:
+				vel_linear = min_vel_linear
+			else:
+				vel_linear = max_vel_linear
+
+			vel_ang = -float(err) / 100
+
+			self.setVelocity(vel_linear, vel_ang)
+
+		self.publish_in_cmd_vel()
+
+	
+	def rotate(self, vel_ang = math.pi/10, clockwise = False):
+		if clockwise:
+			self.setVelocity(0, -vel_ang)
+		else:
+			self.setVelocity(0, vel_ang)
+		
+		self.publish_in_cmd_vel()
+	
+	def go_to_creeper(self, creeper, min_distance = 0.19):
+
+		media, centro, maior_area = creeper.identify_color(self)
+
+		crashed = True if self.getRanges(0) < min_distance else False
+
+		
+		if len(media) != 0 and len(centro) != 0:
+			if abs(media[0] - centro[0]) > 50:
+				if (media[0] > centro[0]):
+					self.setVelocity(0, -0.3)
+				if (media[0] < centro[0]):
+					self.setVelocity(0, 0.3)
+			else:
+				if not crashed:
+					if (media[0] > centro[0]):
+						self.setVelocity(0.1, -0.1)
+					if (media[0] < centro[0]):
+						self.setVelocity(0.1, 0.1)
+		
+		if crashed:
+			self.setVelocity(0, 0)
+			self.move_joints(1.5, 0)
+
+		self.publish_in_cmd_vel()
+
+		return crashed
+
+	
--- scripts/conceitoB.py
+++ scripts/conceitoB.py
@@ -1,179 +1,193 @@
-#! /usr/bin/env python3
-# -*- coding:utf-8 -*-
-
 from __future__ import print_function, division
-
 
 # Para rodar, recomendamos que faça:
 # 
-#    roslaunch my_simulation forca.launch
+#    roslaunch my_simulation pista_u.launch
 #
 # Depois o controlador do braço:
 #
 #    roslaunch mybot_description mybot_control2.launch 	
+
+import time
+import os
 import rospy 
-
 import numpy as np
-
 import cv2
 import cv2.aruco as aruco
+import statsmodels.api as sm
+import math
+import visao_module
 
 from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import LaserScan
 from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
-import statsmodels.api as sm
+from subprocess import call
+from colorama import Fore, Back, Style
+from colorama import init
+from Robot import Robot
+from Creeper import Creeper
 
-import math
+#--------------------------------------------------------------------------
 
-ranges = None
+# DECLARANDO VARIÁVEIS
+
 estagio1 = True
 estagio2 = False
-distancia_passou = False 
-minv = 0
-maxv = 10
-vel_linear = 0.1
-vel_ang = 0
-vel = Twist(Vector3(vel_linear,0,0), Vector3(0,0,vel_ang))
+estagio3 = False
+estagio_creeper = False
+pode  = True
+ultima_placa = 0
+
+
+# # Apresentações
+# print("\n              Insper")
+# time.sleep(1)
+# print("     Engenharia da Computação")
+# time.sleep(1)
+# print("   Robótica Computacional 2021.1")
+# time.sleep(2)
+
+# print(Fore.RED + "\nBem vindo/a ao nosso projeto!")
+# time.sleep(2)
+# print(Fore.WHITE + "Bora conhecer quem gastou muito tempo das suas vidas programando?")
+# time.sleep(2)
+# print("               .")
+# time.sleep(1)
+# print("                 .")
+# time.sleep(1)
+# print("                   .")
+# time.sleep(1)
+# print("Lesgooo")
+# time.sleep(2)
+
+# print(Fore.YELLOW + "\nEstrelando...")
+# time.sleep(1)
+# print(Fore.WHITE + "Fernando Peres Marques Gameiro França " + Fore.YELLOW + "(aka Françinha)")
+# time.sleep(2)
+# print(Fore.WHITE + "Luiza Valezim Augusto Pinto " + Fore.YELLOW + "(aka A Perfeita)")
+# time.sleep(2)
+# print(Fore.WHITE + "Vinicius Grando Eller " + Fore.YELLOW + "(aka Sampas, mas não é o Bot Sampas... longa história)")
+# time.sleep(2)
+
+
+# print(Fore.GREEN + "\nFeat:")
+# time.sleep(1)
+# print(Fore.WHITE + "Fábio Miranda " + Fore.GREEN + "(aka Mirandinha ou Miras)")
+# time.sleep(2)
+# print(Fore.WHITE + "Arnaldo Junior " + Fore.GREEN + "(aka Tiozão)")
+# time.sleep(2)
+# print(Fore.WHITE + "Diego " + Fore.GREEN + "(aka Diego) \n\n" + Fore.WHITE)
+# time.sleep(2)
+
+# # Input
+# print("Então vamos lá:")
+# time.sleep(2)
+# define_id = input("Qual o id que você quer? (22, sim, só tem essa opção) ")
+# define_cor = input(str("Qual cor você quer? (ciano, verde, vermelho) "))
+define_cor = "verde"
+define_id = 21
 
 bridge = CvBridge()
 
-def segmenta_linha_amarela_hsv(hsv):
-    """ REturns a mask within the range"""
-    low = np.array([25, 150, 150])
-    high = np.array([35, 255, 255])
-    mask = cv2.inRange(hsv, low, high)
-    return mask
+# DECLARANDO FUNÇÕES
 
-def morpho_limpa(mask):
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(6,6))
-    mask = cv2.morphologyEx( mask, cv2.MORPH_OPEN, kernel )
-    mask = cv2.morphologyEx( mask, cv2.MORPH_CLOSE, kernel )    
-    return mask
-
-def center_of_mass(mask):
-    """ Retorna uma tupla (cx, cy) que desenha o centro do contorno"""
-    M = cv2.moments(mask)
-    # Usando a expressão do centróide definida em: https://en.wikipedia.org/wiki/Image_moment
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
-    return [int(cX), int(cY)]
-
-def processa_imagem(image):
-    global vel
-    global vel_linear
-    global vel_ang
+def main():
+    global robot
+    global creeper
     global estagio1
     global estagio2
+    global estagio3
+    global estagio_creeper
+    global ultima_placa
+    global pode
 
+    media, centro, maior_area = creeper.identify_color(robot)
+    ids, _ = robot.getAruco()
+    M = robot.get_yellow_moments()
+
+    # Verificando cor e ID dos creepers (ainda sem a parte das cores junto)
+    try:
+        for i in ids:
+            if i == int(define_id) and maior_area > 1500 and pode:
+                estagio1 = False
+                estagio2 = False
+                estagio3 = False
+                estagio_creeper = True
+    except Exception:
+        pass
+    
+    # ESTÁGIOS (1,2,3)
     if estagio1:
-        img = image.copy()
-        img2 = image.copy()
 
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        gray = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2GRAY)
-        mask = segmenta_linha_amarela_hsv(img)
-        mask = morpho_limpa(mask)
+        print("Estágio 1")
 
-        M = cv2.moments(mask)
-        h, w, d = image.shape
+        try:
+            for i in ids:
+                if i == 150:
+                    ultima_placa = "ESQUERDA"
+                elif i == 50:
+                    ultima_placa = "DIREITA"
+        except Exception:
+            pass
 
-        # aruco_dict  = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
-        # # parameters  = aruco.DetectorParameters_create()
-        # # parameters.minDistanceToBorder = 0
-        # # parameters.adaptiveThreshWinSizeMax = 1000
+        robot.follow_yellow_line()
+        
 
-        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict)
-        print(ids)
-        aruco.drawDetectedMarkers(image, corners, ids)
+        if M['m00'] <= 0:
+            estagio1, estagio2 = False, True
+    
+    elif estagio2:
+
+        print (" Estágio 2")
+
+        robot.rotate()
 
         if M['m00'] > 0:
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-            cv2.circle(image, (cx, cy), 10, (0,0,255), -1)
+            estagio2, estagio3 = False, True
+    
+    elif estagio3:
 
-            err = cx - w/2
-            
-            # Diminuir velocidade em curvas
-            if abs(err) > 10:
-                vel_linear = 0.2
-            else:
-                vel_linear = 0.5
+        print("Estágio 3")
 
-            vel_ang = -float(err) / 100
+        img = robot.getImage()
+        w = robot.getWidth()
         
+        if ultima_placa == "ESQUERDA":
+            img[:, 3*w//4:] = [0, 0, 0]
+        else:
+            img[:, :w//4] = [0, 0, 0]
         
-        # Verificando os ids
-        for i in np.squeeze(ids):
-            if i == 50 or i ==150:
-                estagio1 = False
-                estagio2 = True
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    elif estagio2:
-        vel_lin = 0
-        vel_ang = 0
+        robot.follow_yellow_line(img=img)
 
+        if M['m00'] <= 0:
+            estagio1, estagio2 = False, True
 
+    elif estagio_creeper:
 
+        print ("Estágio Creeper")
+        bateu = robot.go_to_creeper(creeper)
+        if bateu:
+            # estagio_creeper, estagio2 = False, True
+            pode = False
 
-def scaneou(dado):
-    global ranges
-    global minv
-    global maxv
-    global distancia_passou
-    print("Faixa valida: ", dado.range_min , " - ", dado.range_max )
-    print("Leituras:")
-    ranges = np.array(dado.ranges).round(decimals=2)
-    distancia = ranges[0]
-    minv = dado.range_min 
-    maxv = dado.range_max
+    cv2.imshow("Main", robot.getImage(original=True))
 
-    if distancia < 0.3:
-        distancia_passou = True
-    else:
-        distancia_passou = False
- 
-# A função a seguir é chamada sempre que chega um novo frame
-def roda_todo_frame(imagem):
-    try:
-        cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
-        processa_imagem(cv_image)
-        cv2.imshow("Camera", cv_image)
-        cv2.waitKey(1)
-    except CvBridgeError as e:
-        print('ex', e)
 
 if __name__=="__main__":
 
     rospy.init_node("q3")
 
-    topico_imagem = "/camera/image/compressed"
-    velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 3 )
-    recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
-    recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
+    robot = Robot()
 
-    while not rospy.is_shutdown():
-        if distancia_passou:
-            vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
-        else:
-            vel = Twist(Vector3(10,0,0), Vector3(0,0,0))
-        # if len(media) != 0 and len(centro) != 0:
-        #     print("Média dos vermelhos: {0}, {1}".format(media[0], media[1]))
-        #     print("Centro dos vermelhos: {0}, {1}".format(centro[0], centro[1]))
+    creeper = Creeper(define_cor, define_id)
 
-        #     if abs(media[0] - centro[0]) > 50:
-        #         if (media[0] > centro[0]):
-        #             vel = Twist(Vector3(0,0,0), Vector3(0,0,-velocidade_angular))
-        #         if (media[0] < centro[0]):
-        #             vel = Twist(Vector3(0,0,0), Vector3(0,0,velocidade_angular))
-        #     else:
-        #         if not distancia_passou:
-        #             if (media[0] > centro[0]):
-        #                 vel = Twist(Vector3(velocidade_linear,0,0), Vector3(0,0,-velocidade_angular))
-        #             if (media[0] < centro[0]):
-        #                 vel = Twist(Vector3(velocidade_linear,0,0), Vector3(0,0,velocidade_angular))
-            
-        velocidade_saida.publish(vel)
-        vel = Twist(Vector3(vel_linear,0,0), Vector3(0,0,vel_ang))
-        velocidade_saida.publish(vel)
-        rospy.sleep(0.1)
+    rospy.sleep(0.1)        
+    
+    try:
+        while not rospy.is_shutdown():
+            main()
+    except rospy.ROSInterruptException:
+        print("     Já entendi... Já pode parar de dar CTRL+C xuxu")
